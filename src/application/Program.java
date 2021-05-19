1853985a5b5942df54ad1108f7552f99763377f9
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2021-05-19 14:00:59 -0300

    Implementacao de Cores, UI parte visual do projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,6 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		board Board = new board(8, 8);
 		
 		ChessMatch chessMatch = new ChessMatch();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import boardgame.Position;
 import boardgame.board;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -27,8 +26,18 @@ public class ChessMatch {
 	}
 	
 	private void initialsetup() {
-		placeNewPiece('b', 6, new Rook(Board, Color.WHITE));
-		placeNewPiece('e', 8, new King(Board, Color.BLACK));
-		placeNewPiece('e', 1,new King(Board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(Board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(Board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(Board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(Board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(Board, Color.WHITE));
+        placeNewPiece('d', 1, new King(Board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(Board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(Board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(Board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(Board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(Board, Color.BLACK));
+        placeNewPiece('d', 8, new King(Board, Color.BLACK));
 	}
 }
